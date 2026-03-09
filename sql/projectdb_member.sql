ddedd8b92324d7cc914c9d00166f7cebf26d6084
CollegeCod <malcolm.ng@lethbridgecollege.ca>
2026-03-08 22:22:28 -0600

    Initial Organization
    Starts work on organizing the project repo.
    
    More files & folders will likely follow suit.

---
+++ sql/projectdb_member.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projectdb
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `MEMB_ID` int NOT NULL AUTO_INCREMENT,
+  `PACKAGE_ID` tinyint unsigned NOT NULL,
+  `MEMB_STATUS_ID` tinyint unsigned NOT NULL,
+  `MEMB_FNAME` varchar(50) NOT NULL,
+  `MEMB_LNAME` varchar(50) NOT NULL,
+  `MEMB_PHONE` varchar(20) DEFAULT NULL,
+  `MEMB_EMAIL` varchar(70) NOT NULL,
+  `MEMB_DOB` date NOT NULL,
+  `MEMB_JOINDATE` date NOT NULL,
+  `START_DATE` date DEFAULT NULL,
+  `END_DATE` date DEFAULT NULL,
+  `NOTES` text,
+  PRIMARY KEY (`MEMB_ID`),
+  UNIQUE KEY `MEMB_EMAIL_UNIQUE` (`MEMB_EMAIL`),
+  KEY `fk_member_package_idx` (`PACKAGE_ID`),
+  KEY `fk_member_memb_status_idx` (`MEMB_STATUS_ID`),
+  CONSTRAINT `fk_member_memb_status` FOREIGN KEY (`MEMB_STATUS_ID`) REFERENCES `memb_status` (`MEMB_STATUS_ID`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_member_package_id` FOREIGN KEY (`PACKAGE_ID`) REFERENCES `packages` (`PACKAGE_ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-25 14:18:06
---
+++ sql/projectdb_payment_transaction.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: projectdb
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `payment_transaction`
+--
+
+DROP TABLE IF EXISTS `payment_transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payment_transaction` (
+  `PMT_ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `INV_ID` int unsigned NOT NULL,
+  `PMT_DATE_TIME` datetime NOT NULL,
+  `PROC_CODE` varchar(40) DEFAULT NULL,
+  `SUCCESS` tinyint(1) NOT NULL,
+  `TOTAL` decimal(7,2) NOT NULL,
+  `FEE` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`PMT_ID`),
+  KEY `fk_payment_invoice_idx` (`INV_ID`),
+  CONSTRAINT `fk_payment_invoice` FOREIGN KEY (`INV_ID`) REFERENCES `invoice` (`INV_ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `payment_transaction`
+--
+
+LOCK TABLES `payment_transaction` WRITE;
+/*!40000 ALTER TABLE `payment_transaction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payment_transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-25 14:18:07
