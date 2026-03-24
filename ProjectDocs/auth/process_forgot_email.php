b9806475f6d91c3ad839823078c28df452837679
Edmund Maloff <edmund.maloff@lethpolytech.ca>
2026-03-24 12:49:54 -0600

    Add files via upload

--- ProjectDocs/auth/process_forgot_email.php
+++ ProjectDocs/auth/process_forgot_email.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 
 validate_csrf_token($_POST["csrf_token"] ?? null);
 
-$user_email = trim($_POST["user_email"] ?? "");
+$user_email = $_SESSION["RESET_EMAIL"] ?? "";
 
 if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
     header("Location: forgot_email.php?status=sent");
@@ -29,7 +29,6 @@ if ($user_record) {
     // 5-minute resend cooldown
     //
     // todo
-    // sends a code, but sees code as invalid for 5 minutes
     $last_sent = $user_record["RESET_CODE_LAST_SENT_AT"];
     if ($last_sent) {
         $seconds_since = time() - strtotime($last_sent);
@@ -46,8 +45,6 @@ if ($user_record) {
     $reset_code_hash = password_hash($reset_code, PASSWORD_DEFAULT);
 
     // Expiry 15 minutes
-    //Time zone ensures that the server clock is the same as the system clock when sending out qr codes
-    date_default_timezone_set("America/Edmonton");
     $expires_at_mysql = date("Y-m-d H:i:s", time() + 900);
 
     set_reset_code_for_user(
